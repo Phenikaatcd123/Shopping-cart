47d14963b8da9f782e11f48ef25c119d99a34fd7
trancongdat123 <120715443+trancongdat123@users.noreply.github.com>
2026-01-22 12:01:35 +0700

    more commits
    commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
-
+import cartIcon from "./assets/icons/shopping-cart-svgrepo-com.svg";
 export default function App() {
   return (
     <>
-      <h1>🛒 Shopping Cart (TypeScript)</h1>
+      <h1><img src={cartIcon} alt="Shopping Cart Icon" /> Welcome to the Shopping Cart</h1>
 
       <div className="layout">
         <ProductList />
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,10 @@
 import Button from "./ui/Button";
 import { useCart } from "../context/CartContext";
 import "../styles/cart.css";
-
+import plusIcon from "./assets/icons/plus-large-svgrepo-com.svg"
+import minusIcon from "./assets/icons/minus-svgrepo-com.svg"
+import trashIcon from "./assets/icons/trash-svgrepo-com.svg"
+import cartIcon from "./assets/icons/shopping-cart-svgrepo-com.svg"
 export default function Cart() {
   const {
     cart,
@@ -13,7 +16,7 @@ export default function Cart() {
   } = useCart();
 
   if (cart.length === 0) {
-    return <p className="cart-empty">Your cart is empty 🛒</p>;
+    return <p className="cart-empty">Your cart is empty <img src={cartIcon} alt="cartIcon" /></p>;
   }
 
   return (
@@ -38,7 +41,7 @@ export default function Cart() {
                 disabled={item.quantity === 0}
                 onClick={() => decrease(item.id)}
               >
-                -
+                <img src={minusIcon} alt="Decrease quantity" />
               </Button>
 
               <span>{item.quantity}</span>
@@ -48,7 +51,7 @@ export default function Cart() {
                 variant="ghost"
                 onClick={() => increase(item.id)}
               >
-                +
+                <img src={plusIcon} alt="Increase quantity" />
               </Button>
             </div>
           </div>
@@ -58,7 +61,7 @@ export default function Cart() {
             variant="danger"
             onClick={() => removeItem(item.id)}
           >
-            Remove
+            <img src={trashIcon} alt="Remove item"/>Remove
           </Button>
         </div>
       ))}
